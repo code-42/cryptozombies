fe8b23895bdb6d321657e9dbfaa07ed5fad44328
Edward Dupre <edwd42@gmail.com>
2018-10-26 22:35:37 -0400

    add file ZombieFactory.sol and declare fields

---
+++ contracts/ZombieFactory.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.19;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    // start here
+    struct Zombie{
+        string name;
+        uint dna;
+    }
+
+}
