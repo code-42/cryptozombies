52cf349153177a8d1c6e6451d364d2747736c52c
Edward Dupre <edwd42@gmail.com>
2018-10-27 08:00:43 -0400

    add event NewZombie

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
+    event NewZombie(uint zombieId, string name, uint dna);
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -14,7 +16,9 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function _createZombie(string _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // uint id = zombies.push() - 1;
+        NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string _str) private view returns (uint){
@@ -26,4 +30,5 @@ contract ZombieFactory {
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
+
 }
