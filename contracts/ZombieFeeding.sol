cdf87485f3be5782894b3298c9cf46202ac12e66
Edward Dupre <edwd42@gmail.com>
2018-10-28 09:56:55 -0400

    require feeding zombie is our zombie and store that zombie on the blockchain

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.19
 
 import "./zombiefactory.sol";
 
-contract ZombieFeeding is ZombieFactory{
+contract ZombieFeeding is ZombieFactory {
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
 
