e81ca4a665ccff0e57365bb66460ab919fa0bfaf
Edward Dupre <edwd42@gmail.com>
2018-10-29 17:40:52 -0400

    add file ownable.sol and import into ZombieFactory

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -1,41 +1,43 @@
 pragma solidity ^0.4.19;
 
-contract ZombieFactory {
+// 1. Import here
+import "./ownable.sol";
+
+// 2. Inherit here:
+contract ZombieFactory is ownable {
 
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
-    struct Zombie{
+    struct Zombie {
         string name;
         uint dna;
     }
- 
+
     Zombie[] public zombies;
 
-    // A mapping is a key-value store
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) internal {
-        
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;        
+        ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns (uint){
+    function _generateRandomDna(string _str) private view returns (uint) {
         uint rand = uint(keccak256(_str));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string _name) public {
-        // ensure only create one zombie
-        require(ownerZombieCount == 0);
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
-    
+
 }
