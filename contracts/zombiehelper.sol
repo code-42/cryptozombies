19486db2b0be301c1dbc3ad942296b0c7d2792a2
Edward Dupre <edwd42@gmail.com>
2018-10-29 19:54:58 -0400

    give zombies new abilities when they level up

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -9,4 +9,20 @@ contract ZombieHelper is ZombieFeeding {
         require(zombies[_zombieId].level >= _level);
         _;
     }
+
+    // zombies level 2 and higher, users can change their name
+    function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
+        // first make sure we own the zombie
+        require(msg.sender == zombieToOwner[_zombieId]);
+        // then we can give it a new name
+        zombies[_zombieId].name = _newName;
+    }
+
+    // zombies level 20 and higher, users can give them custom DNA.
+    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+        // first make sure we own the zombie
+        require(msg.sender == zombieToOwner[_zombieId]);
+        // then we can give it new DNA
+        zombies[_zombieId].dna = _newDna;
+    }
 }
