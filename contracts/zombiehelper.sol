e830222a6f1bdc052dbbfecf20adb0816155c74c
Edward Dupre <edwd42@gmail.com>
2018-10-30 10:33:33 -0400

    add two functions to withdraw and transfer to owner

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -13,6 +13,17 @@ contract ZombieHelper is ZombieFeeding {
         _;
     }
 
+    // 1. Create withdraw function here
+    function withdraw() external onlyOwner {
+        owner.transfer(this.balance);
+    }
+    
+
+    // 2. Create setLevelUpFee function here
+    function setLevelUpFee(uint _fee) external onlyOwner {
+        levelUpFee = _fee;
+    }
+
     // 2. Insert levelUp function here
     function levelUp(uint _zombieId) external payable {
         require(msg.value == levelUpFee);
