d3b9fcde62416cf3c8b00f969b2968e95b77b667
Edward Dupre <edwd42@gmail.com>
2018-10-30 19:57:44 -0400

    add modifier ownerOf() to function definition changeName() and changeDna() and remove require() statement

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -31,17 +31,17 @@ contract ZombieHelper is ZombieFeeding {
     }
 
     // zombies level 2 and higher, users can change their name
-    function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
+    function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
         // first make sure we own the zombie
-        require(msg.sender == zombieToOwner[_zombieId]);
+        // require(msg.sender == zombieToOwner[_zombieId]);
         // then we can give it a new name
         zombies[_zombieId].name = _newName;
     }
 
     // zombies level 20 and higher, users can give them custom DNA.
-    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
         // first make sure we own the zombie
-        require(msg.sender == zombieToOwner[_zombieId]);
+        // require(msg.sender == zombieToOwner[_zombieId]);
         // then we can give it new DNA
         zombies[_zombieId].dna = _newDna;
     }
