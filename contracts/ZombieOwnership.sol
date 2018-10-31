29e80af47182dfebbd7ac2585f3b7f640e81cccb
Edward Dupre <edwd42@gmail.com>
2018-10-31 08:05:15 -0400

    add function definitions of some of ERC721 standard

--- contracts/ZombieOwnership.sol
+++ contracts/ZombieOwnership.sol
@@ -1,9 +1,27 @@
 pragma solidity ^0.4.19;
 
-import "./ZombieAttack.sol";
+import "./zombieattack.sol";
 import "./erc721.sol";
 
-// multiple inheritance
 contract ZombieOwnership is ZombieAttack, ERC721 {
-    
+
+    function balanceOf(address _owner) public view returns (uint256 _balance) {
+        // 1. Return the number of zombies `_owner` has here
+    }
+
+    function ownerOf(uint256 _tokenId) public view returns (address _owner) {
+        // 2. Return the owner of `_tokenId` here
+    }
+
+    function transfer(address _to, uint256 _tokenId) public {
+
+    }
+
+    function approve(address _to, uint256 _tokenId) public {
+
+    }
+
+    function takeOwnership(uint256 _tokenId) public {
+
+    }
 }
