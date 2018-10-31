d1e19b17e7cadefb347c0963b8e261cd84d7428f
Edward Dupre <edwd42@gmail.com>
2018-10-31 08:24:34 -0400

    implement function ZombieOwnership.balanceOf()

--- contracts/ZombieOwnership.sol
+++ contracts/ZombieOwnership.sol
@@ -7,10 +7,12 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
 
     function balanceOf(address _owner) public view returns (uint256 _balance) {
         // 1. Return the number of zombies `_owner` has here
+        return ownerZombieCount[_owner];
     }
 
     function ownerOf(uint256 _tokenId) public view returns (address _owner) {
         // 2. Return the owner of `_tokenId` here
+        
     }
 
     function transfer(address _to, uint256 _tokenId) public {
